44a77d95f862535368357f1a014d729794f0a574
Vladimir Chekholin <kennville@gmail.com>
2021-04-18 20:09:39 +0300

    update configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,6 +4,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/lsm-3d/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
